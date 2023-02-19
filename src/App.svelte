aba7f217a22e3010fa5eca66de20f38bc20590f1
mcgeerdev <mcgeer.devan@gmail.com>
2023-02-19 18:46:06 +0200

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import svelteLogo from './assets/svelte.svg'
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+	<h1>Hi, I am Devan McGeer</h1>
+</main>
+
+<style>
+</style>
