f7c8fe8e1153281070d8dca527e428a07cb2e212
mcgeerdev <mcgeer.devan@gmail.com>
2024-01-08 22:46:11 +0200

    Fixed base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  basepath: "/portfolio/",
+  base: "/portfolio/",
   plugins: [svelte()]
 })
