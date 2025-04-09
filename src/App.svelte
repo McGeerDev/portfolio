4aa2ec67d01d7b82da0b2002026b2ba52f5c24fe
Devan McGeer <devan@meshtrade.co>
2025-04-09 14:00:24 +0200

    Added robots.txt for crawling

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,27 @@
 <script lang="ts">
+	import { Navigation } from '@skeletonlabs/skeleton-svelte';
+	// Icons
+	import IconFolder from '@lucide/svelte/icons/folder';
+	import IconImage from '@lucide/svelte/icons/image';
+	import IconMusic from '@lucide/svelte/icons/music';
+	import IconVideo from '@lucide/svelte/icons/video';
+  
+	// State
+	let value = $state('files');
 </script>
+  
+<div class="h-screen grid grid-rows-[auto-1fr-auto]">
+	<header class="bg-red p-4">
+		HEADER
+	</header>
+	<main class="bg-green p-4 space-y-4">
+		CONTENT
+	</main>
+	<footer class="bg-blue p-4">FOOTER</footer>
 
-<main>
-	<h1> I am Piet</h1>
-</main>
+</div>
 
-<style>
-</style>
+
+
+
+
