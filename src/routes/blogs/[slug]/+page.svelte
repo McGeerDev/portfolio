4fa7fe5258bcfd51fb30934c39e116c07cdcb6dd
Devan McGeer <devan@meshtrade.co>
2025-07-12 22:02:54 +0200

    feat(blog): Added dynamic routing to blogs
    Added basic error page
    Added msx content get

--- src/routes/blogs/[slug]/+page.svelte
+++ src/routes/blogs/[slug]/+page.svelte
@@ -1,15 +1,21 @@
 <script lang="ts">
 	import type { PageProps } from './$types';
-
 	let { data }: PageProps = $props();
+  console.log({data})
 </script>
 
 <div class="py-16 sm:py-24 text-center">
   <h1 class="font-lexend-zetta text-4xl sm:text-6xl md:text-7xl font-light tracking-widest leading-tight">
-    {data.title}
+    {data.metadata.title}
   </h1>
   <p class="mt-6 text-lg sm:text-xl md:text-2xl tracking-[0.3em] sm:tracking-[0.4em] text-gray-700">
-    {data.content}
+    {data.metadata.description}
   </p>
+</div>
+
+<label for="content">THIS IS THE BLOG CONTENT</label>
+<div class="border-4 border-amber-200" id="content">
+
+<data.content/>
 </div>
 
--- src/routes/blogs/[slug]/+page.ts
+++ src/routes/blogs/[slug]/+page.ts
@@ -1,13 +1,15 @@
 import { error } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
-export const load: PageLoad = ({ params }) => {
-	if (params.slug === 'hello') {
-		return {
-			title: 'Hello world!',
-			content: 'Welcome to our blog. Lorem ipsum dolor sit amet...'
-		};
-	}
-
-	error(404, 'Not found');
-};
+export const load: PageLoad = async ({params}) => {
+        console.log({params})
+    try {
+        const blogData = await import(`$content/blogs/${params.slug}.svx`)
+        return {
+            content: blogData.default,
+            metadata: blogData.metadata
+        }
+    } catch (e) {
+        error(404,`Page not found: ${params.slug}`)
+    }
+}
