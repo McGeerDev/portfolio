474b7cf01b4ce8cf11bfdda06120de37799d394f
Devan McGeer <devan@meshtrade.co>
2025-07-20 02:23:14 +0200

    feature(hero): Added hero banner (#19)
    Imported hero into home, blog, and blogs pages

--- src/routes/blogs/+page.svelte
+++ src/routes/blogs/+page.svelte
@@ -1,12 +1,6 @@
-<div class="py-16 text-center sm:py-24">
-	<!-- Hero Section -->
-	<h1
-		class="font-lexend-zetta text-4xl leading-tight font-light tracking-widest sm:text-6xl md:text-7xl"
-	>
-		DEVAN<br />MCGEER
-	</h1>
-	<p class="mt-6 text-lg tracking-[0.3em] text-gray-700 sm:text-xl sm:tracking-[0.4em] md:text-2xl">
-		SITE RELIABILITY ENGINEER
-	</p>
-	<a href="/blogs/first" class="tracking-widest hover:text-gray-600">FIRST</a>
-</div>
+<script>
+	import Hero from '$lib/components/hero.svelte'
+</script>
+
+<Hero title="Devan McGeer" subtitle="Blog"/>
+<a href="/blogs/first" class="tracking-widest hover:text-gray-600">FIRST</a>
